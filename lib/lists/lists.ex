687df39c1eba7c6889b4402c467dc16471d8a426
Diego Borges <drborges.cic@gmail.com>
2017-02-20 00:12:59 -0300

    Use pipe operator on Spikes.Lists.any? and Spikes.Lists.at

--- lib/lists/lists.ex
+++ lib/lists/lists.ex
@@ -11,12 +11,12 @@ defmodule Spikes.Lists do
 
   def at(list, 0), do: head(list)
   def at(list, index) do
-    at(tail(list), index-1)
+    tail(list) |> at(index-1)
   end
 
   def any?([value | _], value), do: true
   def any?(list, value) do
-    any?(tail(list), value)
+    tail(list) |> any?(value)
   end
 
   def all?([value], value), do: true
